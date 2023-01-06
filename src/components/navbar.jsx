9728c1e18232c2910c67071924de9adebe4f5a77
Rindho Ananta Samat <rindho.ananta@ti.ukdw.ac.id>
2023-01-06 22:11:53 +0800

    deploy vercel

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,34 @@
-const Navbar = () => {
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap"
+import logo from "../assets/images/logo.svg"
+
+const NavbarComponent = () => {
 	return (
-		<h1>Test</h1>
+		<Navbar bg="light" expand="lg">
+			<Container fluid>
+				<Navbar.Brand href="#home">
+					<img src="./logo192.png" width="30" height="30" className="d-inline-block align-top" alt="..." />
+					React-Bootstrap
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="ms-auto">
+						<Nav.Link href="#home">Home</Nav.Link>
+						<Nav.Link href="#link">Link</Nav.Link>
+						<NavDropdown title="Dropdown" id="basic-nav-dropdown">
+							<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.2">
+								Another action
+							</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+							<NavDropdown.Divider />
+							<NavDropdown.Item href="#action/3.4">
+								Separated link
+							</NavDropdown.Item>
+						</NavDropdown>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
 	)
 }
-export default Navbar
+export default NavbarComponent
