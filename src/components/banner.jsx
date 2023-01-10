e8e406ff83da6053bae7b066bcc21496a41f7cef
Rindho Ananta Samat <rindho.ananta@ti.ukdw.ac.id>
2023-01-10 21:43:43 +0700

    test

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -37,6 +37,7 @@ const Banner = () => {
           </div>
           <div className="img">
             <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" alt="" />
+            oi
           </div>
         </OwlCarousel>
       </div>
