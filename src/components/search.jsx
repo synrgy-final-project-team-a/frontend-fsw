0aa32ba8dc123a42b55497087c920992468a23cc
fayyadhsr <outofthisworldyo@gmail.com>
2023-01-10 20:56:07 +0700

    delete import row,col,form in search.jsx

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,8 +1,4 @@
 import { useState } from "react";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { Form } from "react-bootstrap";
 
 const Search = () => {
 
