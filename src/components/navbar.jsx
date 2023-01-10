5866ce04fd493d31f2654a46e8f4672c10b72900
Rindho Ananta Samat <rindho.ananta@ti.ukdw.ac.id>
2023-01-10 22:00:51 +0700

    change

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,7 @@ const NavbarComponent = () => {
 			<Container>
 				<Navbar.Brand href="#home">
 					<img src="./logo192.png" width="30" height="30" className="d-inline-block align-top mx-2" alt="..." />
-					KOSANKUU
+					KOSANKU
 				</Navbar.Brand>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
