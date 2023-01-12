db6dec092217939425a7c84848d7ce94d5f7c85c
Rindho Ananta Samat <rindho.ananta@ti.ukdw.ac.id>
2023-01-12 22:24:57 +0700

    ada confloct

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -4,10 +4,13 @@ import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 
 const Banner = () => {
+  let owlNavPrev = '<h1>Prev</h1>'
+  let owlNavNext = '<h1>Next</h1>'
+
   return (
     <div>
       <div class="">
-        <OwlCarousel items={1} className="owl-theme banner" loop dots={false} nav>
+        <OwlCarousel items={1} className="owl-theme banner" loop dots={false} nav navText={[owlNavPrev, owlNavNext]}>
           <div className="img-banner" class="img-fluid">
             <img src="https://static.mamikos.com/uploads/cache/data/event/2023-01-11/GWQermw5-540x720.jpg" alt="" />
           </div>
