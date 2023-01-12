47d33a085493c48fda317994df93c183ceb1dd55
Rindho Ananta Samat <73924542+yerndd@users.noreply.github.com>
2023-01-12 20:24:15 +0700

    Testing (#15)
    * env test
    
    * deploy test
    
    * Landing (#6)
    
    * add :package: bootstrap & react-bootstrap
    
    * change to scss
    
    * add routes in App.jsx
    
    * add owl carousel
    
    * edit owl carousel
    
    * deploy vercel
    
    * deploy vercel
    
    * add kostByCity
    
    * add KostByCampus
    
    * deploy fix
    
    Co-authored-by: Ryan Jonathan <ryanjnthan@gmail.com>
    
    * deploy production
    
    * deploy dev
    
    * deploy fix
    
    * Delete deploy.yaml
    
    * deploy fix
    
    * gitignore
    
    * Delete deploy.yaml
    
    * deploy fix
    
    * deploy fix
    
    * deploy dev fix
    
    * change workflow name
    
    * fix deploy (#10)
    
    * add :package: bootstrap & react-bootstrap
    
    * change to scss
    
    * add routes in App.jsx
    
    * add owl carousel
    
    * edit owl carousel
    
    * deploy vercel
    
    * deploy vercel
    
    * add kostByCity
    
    * add KostByCampus
    
    * deploy fix
    
    * fix deploy
    
    Co-authored-by: Ryan Jonathan <ryanjnthan@gmail.com>
    
    * Landing Page Without Design (#12)
    
    * add :package: bootstrap & react-bootstrap
    
    * change to scss
    
    * add routes in App.jsx
    
    * add owl carousel
    
    * edit owl carousel
    
    * deploy vercel
    
    * deploy vercel
    
    * add kostByCity
    
    * add KostByCampus
    
    * deploy fix
    
    * fix deploy
    
    * add footer component
    
    * search
    
    * fix banner & kostBy
    
    * add footer
    
    Co-authored-by: Ryan Jonathan <ryanjnthan@gmail.com>
    Co-authored-by: fayyadhsr <outofthisworldyo@gmail.com>
    
    * test deploy (#13)
    
    * add :package: bootstrap & react-bootstrap
    
    * change to scss
    
    * add routes in App.jsx
    
    * add owl carousel
    
    * edit owl carousel
    
    * deploy vercel
    
    * deploy vercel
    
    * add kostByCity
    
    * add KostByCampus
    
    * deploy fix
    
    * fix deploy
    
    * add footer component
    
    * search
    
    * fix banner & kostBy
    
    * add footer
    
    * delete import row,col,form in search.jsx
    
    Co-authored-by: Ryan Jonathan <ryanjnthan@gmail.com>
    Co-authored-by: fayyadhsr <outofthisworldyo@gmail.com>
    
    * test action to dev
    
    * change workflow
    
    * change workflow
    
    * change workflow
    
    * change workflow
    
    * test
    
    * testt
    
    * change
    
    Co-authored-by: Ryan Jonathan <ryanjnthan@gmail.com>
    Co-authored-by: fayyadhsr <outofthisworldyo@gmail.com>
    Co-authored-by: Reja <rezanurrochmat@gmail.com>

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,15 +1,19 @@
 import Banner from "../components/banner";
+import FooterComponent from "../components/footer";
 import KostByCampus from "../components/kostByCampus";
 import KostByCity from "../components/kostByCity";
 import Navbar from "../components/navbar";
+import Search from "../components/search";
 
 const Landing = () => {
   return (
     <>
       <Navbar />
       <Banner />
+      <Search />
       <KostByCity />
       <KostByCampus />
+      <FooterComponent />
     </>
   );
 };
