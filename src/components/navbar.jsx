767d4db61f5ef1cd48a0672ca499b9c535d5b092
Rindho Ananta Samat <rindho.ananta@ti.ukdw.ac.id>
2023-01-13 13:18:44 +0700

    finished fix

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,17 +4,17 @@ const NavbarComponent = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand href="/">
           <img src="./logo192.png" width="30" height="30" className="d-inline-block align-top mx-2" alt="..." />
           KOSANKU
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto align-items-center gap-36">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="#carikos">Cari Kos</Nav.Link>
             <Nav.Link href="#sewakos">Sewa Kos</Nav.Link>
-            <Button as={Nav.Link} href="#login" variant="outline-success" className="btn-login">
+            <Button as={Nav.Link} href="/login" variant="outline-success" className="btn-login">
               Masuk
             </Button>
           </Nav>
