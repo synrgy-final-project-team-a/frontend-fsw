f93631cd16ba94718cd7b198ea9240fa1c729c44
Rindho Ananta Samat <rindho.ananta@ti.ukdw.ac.id>
2023-01-17 15:39:18 +0700

    layout penyewa

---
+++ src/pages/penyewa/dashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import PenyewaLayout from "../../layouts/admin.layout"
+
+function Dashboard() {
+    return (
+        <>
+            <PenyewaLayout>
+                <h1>asdasdaasdasdasdsd</h1>
+            </PenyewaLayout>
+        </>
+    );
+};
+
+export default Dashboard;
