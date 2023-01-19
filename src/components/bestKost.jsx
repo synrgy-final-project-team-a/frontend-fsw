31474c6bc7639e84221d58c84778b9fb7893c14a
Rindho Ananta Samat <rindho.ananta@ti.ukdw.ac.id>
2023-01-19 21:39:55 +0700

    prepare test

--- src/components/bestKost.jsx
+++ src/components/bestKost.jsx
@@ -10,7 +10,7 @@ function BestKost() {
     <div className="ms-2">
       <h2>Kos Terbaik dari Kami</h2>
       <Container>
-        <Row lg={5}>
+        <Row xs={1} lg={5}>
           <Col className="d-flex">
             <Card className="mb-2 flex-fill">
               <Card.Img variant="top" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" />
@@ -22,14 +22,14 @@ function BestKost() {
                   Kos Ajimumpung
                 </Badge>{" "}
                 {/* <Card.Title>Card Title</Card.Title> */}
-                <Card.Text className="">
-                  <p style={{ margin: 0 }}>Indekos Bu Sapri</p>
-                  <p style={{ margin: 0 }} className="fw-bold">
-                    Jakarta
-                  </p>
-                  <p style={{ margin: 0, fontSize: "12px" }} className="text-muted">
-                    K. Mandi Dalam, Kloset Duduk
-                  </p>
+                <Card.Text className="m-0">
+                  Indekos Bu Sapri
+                </Card.Text>
+                <Card.Text className="fw-bold m-0">
+                  Jakarta
+                </Card.Text>
+                <Card.Text className="text-muted">
+                  K. Mandi Dalam, Kloset Duduk
                 </Card.Text>
                 <Button size="sm" variant="primary">
                   Lihat Detail
@@ -37,114 +37,6 @@ function BestKost() {
               </Card.Body>
             </Card>
           </Col>
-          <Col className="d-flex">
-            <Card className="ms-2 mb-2 flex-fill">
-              <Card.Img variant="top" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" />
-              <Card.Body>
-                <Badge bg="secondary" text="light">
-                  Putri
-                </Badge>{" "}
-                <Badge bg="secondary" text="light">
-                  Kos Ajimumpung
-                </Badge>{" "}
-                {/* <Card.Title>Card Title</Card.Title> */}
-                <Card.Text className="">
-                  <p style={{ margin: 0 }}>Indekos Bu Sapri</p>
-                  <p style={{ margin: 0 }} className="fw-bold">
-                    Jakarta
-                  </p>
-                  <p style={{ margin: 0, fontSize: "12px" }} className="text-muted">
-                    K. Mandi Dalam, Kloset Duduk
-                  </p>
-                </Card.Text>
-                <Button size="sm" variant="primary">
-                  Lihat Detail
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="d-flex">
-            <Card className="ms-2 mb-2 flex-fill">
-              <Card.Img variant="top" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" />
-              <Card.Body>
-                <Badge bg="secondary" text="light">
-                  Putri
-                </Badge>{" "}
-                <Badge bg="secondary" text="light">
-                  Kos Ajimumpung
-                </Badge>{" "}
-                {/* <Card.Title>Card Title</Card.Title> */}
-                <Card.Text className="">
-                  <p style={{ margin: 0 }}>Indekos Bu Sapri</p>
-                  <p style={{ margin: 0 }} className="fw-bold">
-                    Jakarta
-                  </p>
-                  <p style={{ margin: 0, fontSize: "12px" }} className="text-muted">
-                    K. Mandi Dalam, Kloset Duduk
-                  </p>
-                </Card.Text>
-                <Button size="sm" variant="primary">
-                  Lihat Detail
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="d-flex">
-            <Card className="ms-2 mb-2 flex-fill">
-              <Card.Img variant="top" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" />
-              <Card.Body>
-                <Badge bg="secondary" text="light">
-                  Putri
-                </Badge>{" "}
-                <Badge bg="secondary" text="light">
-                  Kos Ajimumpung
-                </Badge>{" "}
-                {/* <Card.Title>Card Title</Card.Title> */}
-                <Card.Text className="">
-                  <p style={{ margin: 0 }}>Indekos Bu Sapri</p>
-                  <p style={{ margin: 0 }} className="fw-bold">
-                    Jakarta
-                  </p>
-                  <p style={{ margin: 0, fontSize: "12px" }} className="text-muted">
-                    K. Mandi Dalam, Kloset Duduk
-                  </p>
-                </Card.Text>
-                <Button size="sm" variant="primary">
-                  Lihat Detail
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="d-flex">
-            <Card className="ms-2 mb-2 flex-fill">
-              <Card.Img variant="top" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" />
-              <Card.Body>
-                <Badge bg="secondary" text="light">
-                  Putri
-                </Badge>{" "}
-                <Badge bg="secondary" text="light">
-                  Kos Ajimumpung
-                </Badge>{" "}
-                {/* <Card.Title>Card Title</Card.Title> */}
-                <Card.Text className="">
-                  <p style={{ margin: 0 }}>Indekos Bu Sapri</p>
-                  <p style={{ margin: 0 }} className="fw-bold">
-                    Jakarta
-                  </p>
-                  <p style={{ margin: 0, fontSize: "12px" }} className="text-muted">
-                    K. Mandi Dalam, Kloset Duduk
-                  </p>
-                </Card.Text>
-                <Button size="sm" variant="primary">
-                  Lihat Detail
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-      <Container>
-        <Row lg={5}>
           <Col className="d-flex">
             <Card className="mb-2 flex-fill">
               <Card.Img variant="top" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" />
@@ -156,14 +48,14 @@ function BestKost() {
                   Kos Ajimumpung
                 </Badge>{" "}
                 {/* <Card.Title>Card Title</Card.Title> */}
-                <Card.Text className="">
-                  <p style={{ margin: 0 }}>Indekos Bu Sapri</p>
-                  <p style={{ margin: 0 }} className="fw-bold">
-                    Jakarta
-                  </p>
-                  <p style={{ margin: 0, fontSize: "12px" }} className="text-muted">
-                    K. Mandi Dalam, Kloset Duduk
-                  </p>
+                <Card.Text className="m-0">
+                  Indekos Bu Sapri
+                </Card.Text>
+                <Card.Text className="fw-bold m-0">
+                  Jakarta
+                </Card.Text>
+                <Card.Text className="text-muted">
+                  K. Mandi Dalam, Kloset Duduk
                 </Card.Text>
                 <Button size="sm" variant="primary">
                   Lihat Detail
@@ -172,7 +64,7 @@ function BestKost() {
             </Card>
           </Col>
           <Col className="d-flex">
-            <Card className="ms-2 mb-2 flex-fill">
+            <Card className="mb-2 flex-fill">
               <Card.Img variant="top" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" />
               <Card.Body>
                 <Badge bg="secondary" text="light">
@@ -182,14 +74,14 @@ function BestKost() {
                   Kos Ajimumpung
                 </Badge>{" "}
                 {/* <Card.Title>Card Title</Card.Title> */}
-                <Card.Text className="">
-                  <p style={{ margin: 0 }}>Indekos Bu Sapri</p>
-                  <p style={{ margin: 0 }} className="fw-bold">
-                    Jakarta
-                  </p>
-                  <p style={{ margin: 0, fontSize: "12px" }} className="text-muted">
-                    K. Mandi Dalam, Kloset Duduk
-                  </p>
+                <Card.Text className="m-0">
+                  Indekos Bu Sapri
+                </Card.Text>
+                <Card.Text className="fw-bold m-0">
+                  Jakarta
+                </Card.Text>
+                <Card.Text className="text-muted">
+                  K. Mandi Dalam, Kloset Duduk
                 </Card.Text>
                 <Button size="sm" variant="primary">
                   Lihat Detail
@@ -198,7 +90,7 @@ function BestKost() {
             </Card>
           </Col>
           <Col className="d-flex">
-            <Card className="ms-2 mb-2 flex-fill">
+            <Card className="mb-2 flex-fill">
               <Card.Img variant="top" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" />
               <Card.Body>
                 <Badge bg="secondary" text="light">
@@ -208,14 +100,14 @@ function BestKost() {
                   Kos Ajimumpung
                 </Badge>{" "}
                 {/* <Card.Title>Card Title</Card.Title> */}
-                <Card.Text className="">
-                  <p style={{ margin: 0 }}>Indekos Bu Sapri</p>
-                  <p style={{ margin: 0 }} className="fw-bold">
-                    Jakarta
-                  </p>
-                  <p style={{ margin: 0, fontSize: "12px" }} className="text-muted">
-                    K. Mandi Dalam, Kloset Duduk
-                  </p>
+                <Card.Text className="m-0">
+                  Indekos Bu Sapri
+                </Card.Text>
+                <Card.Text className="fw-bold m-0">
+                  Jakarta
+                </Card.Text>
+                <Card.Text className="text-muted">
+                  K. Mandi Dalam, Kloset Duduk
                 </Card.Text>
                 <Button size="sm" variant="primary">
                   Lihat Detail
@@ -224,7 +116,7 @@ function BestKost() {
             </Card>
           </Col>
           <Col className="d-flex">
-            <Card className="ms-2 mb-2 flex-fill">
+            <Card className="mb-2 flex-fill">
               <Card.Img variant="top" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" />
               <Card.Body>
                 <Badge bg="secondary" text="light">
@@ -234,14 +126,14 @@ function BestKost() {
                   Kos Ajimumpung
                 </Badge>{" "}
                 {/* <Card.Title>Card Title</Card.Title> */}
-                <Card.Text className="">
-                  <p style={{ margin: 0 }}>Indekos Bu Sapri</p>
-                  <p style={{ margin: 0 }} className="fw-bold">
-                    Jakarta
-                  </p>
-                  <p style={{ margin: 0, fontSize: "12px" }} className="text-muted">
-                    K. Mandi Dalam, Kloset Duduk
-                  </p>
+                <Card.Text className="m-0">
+                  Indekos Bu Sapri
+                </Card.Text>
+                <Card.Text className="fw-bold m-0">
+                  Jakarta
+                </Card.Text>
+                <Card.Text className="text-muted">
+                  K. Mandi Dalam, Kloset Duduk
                 </Card.Text>
                 <Button size="sm" variant="primary">
                   Lihat Detail
@@ -250,7 +142,7 @@ function BestKost() {
             </Card>
           </Col>
           <Col className="d-flex">
-            <Card className="ms-2 mb-2 flex-fill">
+            <Card className="mb-2 flex-fill">
               <Card.Img variant="top" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" />
               <Card.Body>
                 <Badge bg="secondary" text="light">
@@ -260,14 +152,118 @@ function BestKost() {
                   Kos Ajimumpung
                 </Badge>{" "}
                 {/* <Card.Title>Card Title</Card.Title> */}
-                <Card.Text className="">
-                  <p style={{ margin: 0 }}>Indekos Bu Sapri</p>
-                  <p style={{ margin: 0 }} className="fw-bold">
-                    Jakarta
-                  </p>
-                  <p style={{ margin: 0, fontSize: "12px" }} className="text-muted">
-                    K. Mandi Dalam, Kloset Duduk
-                  </p>
+                <Card.Text className="m-0">
+                  Indekos Bu Sapri
+                </Card.Text>
+                <Card.Text className="fw-bold m-0">
+                  Jakarta
+                </Card.Text>
+                <Card.Text className="text-muted">
+                  K. Mandi Dalam, Kloset Duduk
+                </Card.Text>
+                <Button size="sm" variant="primary">
+                  Lihat Detail
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col className="d-flex">
+            <Card className="mb-2 flex-fill">
+              <Card.Img variant="top" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" />
+              <Card.Body>
+                <Badge bg="secondary" text="light">
+                  Putri
+                </Badge>{" "}
+                <Badge bg="secondary" text="light">
+                  Kos Ajimumpung
+                </Badge>{" "}
+                {/* <Card.Title>Card Title</Card.Title> */}
+                <Card.Text className="m-0">
+                  Indekos Bu Sapri
+                </Card.Text>
+                <Card.Text className="fw-bold m-0">
+                  Jakarta
+                </Card.Text>
+                <Card.Text className="text-muted">
+                  K. Mandi Dalam, Kloset Duduk
+                </Card.Text>
+                <Button size="sm" variant="primary">
+                  Lihat Detail
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col className="d-flex">
+            <Card className="mb-2 flex-fill">
+              <Card.Img variant="top" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" />
+              <Card.Body>
+                <Badge bg="secondary" text="light">
+                  Putri
+                </Badge>{" "}
+                <Badge bg="secondary" text="light">
+                  Kos Ajimumpung
+                </Badge>{" "}
+                {/* <Card.Title>Card Title</Card.Title> */}
+                <Card.Text className="m-0">
+                  Indekos Bu Sapri
+                </Card.Text>
+                <Card.Text className="fw-bold m-0">
+                  Jakarta
+                </Card.Text>
+                <Card.Text className="text-muted">
+                  K. Mandi Dalam, Kloset Duduk
+                </Card.Text>
+                <Button size="sm" variant="primary">
+                  Lihat Detail
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col className="d-flex">
+            <Card className="mb-2 flex-fill">
+              <Card.Img variant="top" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" />
+              <Card.Body>
+                <Badge bg="secondary" text="light">
+                  Putri
+                </Badge>{" "}
+                <Badge bg="secondary" text="light">
+                  Kos Ajimumpung
+                </Badge>{" "}
+                {/* <Card.Title>Card Title</Card.Title> */}
+                <Card.Text className="m-0">
+                  Indekos Bu Sapri
+                </Card.Text>
+                <Card.Text className="fw-bold m-0">
+                  Jakarta
+                </Card.Text>
+                <Card.Text className="text-muted">
+                  K. Mandi Dalam, Kloset Duduk
+                </Card.Text>
+                <Button size="sm" variant="primary">
+                  Lihat Detail
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col className="d-flex">
+            <Card className="mb-2 flex-fill">
+              <Card.Img variant="top" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" />
+              <Card.Body>
+                <Badge bg="secondary" text="light">
+                  Putri
+                </Badge>{" "}
+                <Badge bg="secondary" text="light">
+                  Kos Ajimumpung
+                </Badge>{" "}
+                {/* <Card.Title>Card Title</Card.Title> */}
+                <Card.Text className="m-0">
+                  Indekos Bu Sapri
+                </Card.Text>
+                <Card.Text className="fw-bold m-0">
+                  Jakarta
+                </Card.Text>
+                <Card.Text className="text-muted">
+                  K. Mandi Dalam, Kloset Duduk
                 </Card.Text>
                 <Button size="sm" variant="primary">
                   Lihat Detail
