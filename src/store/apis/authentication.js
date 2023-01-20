e58d4bb6239b33136751b283403a13fb72b13783
Rindho Ananta Samat <rindho.ananta@ti.ukdw.ac.id>
2023-01-20 16:02:16 +0700

    resolve conflict

--- src/store/apis/authentication.js
+++ src/store/apis/authentication.js
@@ -54,13 +54,8 @@ export const {
 	useRegisterMutation,
 	useResendOtpMutation,
 	useLoginMutation,
-<<<<<<< HEAD
 	useForgotPasswordMutation,
 	useChangePasswordMutation
-=======
-	useResendOtpMutation,
-	useForgotPasswordMutation
->>>>>>> 5190bcd1e80e47a4256ac1adc37f82ff8dde4d51
 } = authApi
 
 export default authApi
