08d4a2bf916e7799a9e94f2e19ed87008c24b40a
Ryan Jonathan <ryanjnthan@gmail.com>
2023-01-24 21:31:59 +0700

    fix route

--- src/routes/pencari.js
+++ src/routes/pencari.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import React from "react";
 import Landing from "../pages/pencari/landing";
-import Login from "../pages/pencari/login";
-import Register from "../pages/pencari/register";
-import ForgetPass from "../pages/pencari/forgetPassword";
 import DetailKos from "../pages/pencari/detailKos";
 
 const PencariRoutes = [
@@ -23,44 +19,10 @@ const PencariRoutes = [
     element: <Landing />,
   },
   {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    path: "/verif-email",
-    element: <ForgetPass />,
-  },
-  {
     name: "Detail Kos",
     path: "/detail-kos",
     element: <DetailKos />,
   },
 ];
-=======
-import React from "react"
-import Landing from "../pages/pencari/landing"
-
-const PencariRoutes = [
-	{
-		name: "Home",
-		path: "/",
-		element: <Landing />,
-	},
-	{
-		name: "Cari Kos",
-		path: "/cari-kos",
-		element: <Landing />,
-	},
-	{
-		name: "Sewa Kos",
-		path: "/sewa-kos",
-		element: <Landing />,
-	},
-]
->>>>>>> 2711a2e108edc9b317b1860b7bf751e5274e71d5
 
 export default PencariRoutes;
