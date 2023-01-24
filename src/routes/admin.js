10a5c887a3eee64caaf4c5c1ffeac096ceebb3c7
Rindho Ananta Samat <rindho.ananta@ti.ukdw.ac.id>
2023-01-24 19:03:21 +0700

    vercel build fixing

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Dashboard from "../pages/admin/dashboard"
 
-import Test from "../pages/admin/test"
 import DetilUser from "../pages/admin/users/detiluser"
 import KelolaUser from "../pages/admin/users/listuser"
 import TambahUser from "../pages/admin/users/tambahUser"
@@ -27,11 +26,6 @@ const AdminRoutes = [
 				name: "Dashboard",
 				path: "",
 				element: <Dashboard />,
-			},
-			{
-				name: "Test",
-				path: "/test",
-				element: <Test />,
 			},
 			{
 				path: "/users",
