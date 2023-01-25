685ad9ba3839e0ac4173590ef3d1dba9d1a126cd
Rindho Ananta Samat <73924542+yerndd@users.noreply.github.com>
2023-01-26 01:54:46 +0700

    Authentication All Done (#37)
    * add list users and detail
    
    * routes admin test
    
    * add tambah user page
    
    * edit tambah user page
    
    * resolve conflict
    
    * resolve conflict
    
    * register fix
    
    * routes fix admin
    
    * logout & delete user
    
    * logout fix & detail user
    
    * fix url access
    
    Co-authored-by: fayyadhsr <outofthisworldyo@gmail.com>

--- src/layouts/penyewa.layout.jsx
+++ src/layouts/penyewa.layout.jsx
@@ -1,9 +1,34 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
 import FooterComponent from "../components/footer"
 import NavbarComponent from "../components/navbar"
 import PenyewaRoutes from "../routes/penyewa"
+import { emptyEmail, emptyToken } from "../store/slices/authSlice"
+import { emptyUser } from "../store/slices/userSlice"
 
 const PenyewaLayout = ({ children }) => {
+	const dispatch = useDispatch()
+	const navigate = useNavigate()
+
+	const token = useSelector(state => state.auth.token)
+
+	useEffect(() => {
+		if (Object.keys(token).length === 0) {
+			dispatch(emptyToken())
+			dispatch(emptyEmail())
+			dispatch(emptyUser())
+			navigate('/login')
+		} else {
+			if (!token.role.includes('ROLE_TN')) {
+				dispatch(emptyToken())
+				dispatch(emptyEmail())
+				dispatch(emptyUser())
+				navigate('/login')
+			}
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
 
 	return (
 		<>
