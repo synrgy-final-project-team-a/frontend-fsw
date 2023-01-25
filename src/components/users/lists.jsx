7207ffc3a83b4773eb1920b28e27517ecacd7c99
Rindho Ananta Samat <73924542+yerndd@users.noreply.github.com>
2023-01-25 22:44:47 +0700

    Authentication login logout fix (#36)
    * add :package: bootstrap & react-bootstrap
    
    * change to scss
    
    * add routes in App.jsx
    
    * new
    
    * add code
    
    * add owl carousel
    
    * edit owl carousel
    
    * deploy vercel
    
    * deploy vercel
    
    * add kostByCity
    
    * add KostByCampus
    
    * deploy fix
    
    * fix deploy
    
    * add footer component
    
    * bikin register page
    
    * search
    
    * fix banner & kostBy
    
    * add footer
    
    * delete import row,col,form in search.jsx
    
    * change
    
    * change
    
    * login blm fix
    
    * edit sedikit navbar
    
    * add login
    
    * add integrasi API
    
    * page resister 90
    
    * integrasi API register seeker
    
    * perbaikan
    
    * example
    
    * add testimonial & yg lain
    
    * edit navbar
    
    * ada confloct
    
    * kelar
    
    * search
    
    * fix bestKost & kostByLocation
    
    * fix testi card to become center
    
    * add login component
    
    * finished fix
    
    * add pencari layout
    
    * update layout
    
    * add style
    
    * add image
    
    * add list users and detail
    
    * routes admin test
    
    * update
    
    * update style
    
    * update again
    
    * add tambah user page
    
    * add verif email sukses
    
    * add detail
    
    * register remake
    
    * register fix
    
    * register routes
    
    * edit tambah user page
    
    * responsive learning
    
    * scss variables & register
    
    * register final
    
    * final register
    
    * auth progress
    
    * prepare test
    
    * testing register
    
    * add firstname last name
    
    * register final
    
    * edit navbar component
    
    * forgot password finishing
    
    * forgot password fix
    
    * resolve conflict
    
    * login finish
    
    * edit navbar (belom fix) and footer
    
    * profile dropdown
    
    * vercel build fixing
    
    * resolve conflict
    
    * resolve conflict
    
    * register fix
    
    * routes fix admin
    
    * fix register
    
    * logout & delete user
    
    * logout fix & detail user
    
    * fix logout
    
    Co-authored-by: ditan91 <ditanoviyanti91@gmail.com>
    Co-authored-by: Ryan Jonathan <ryanjnthan@gmail.com>
    Co-authored-by: Ghifari <ghifargip7@gmail.com>
    Co-authored-by: fayyadhsr <outofthisworldyo@gmail.com>

--- src/components/users/lists.jsx
+++ src/components/users/lists.jsx
@@ -1,62 +1,146 @@
-import { useEffect } from "react"
-import { Table, Button, ButtonGroup } from "react-bootstrap"
+import { useEffect, useState } from "react"
+import { Table, Button, ButtonGroup, Alert } from "react-bootstrap"
 import { useSelector } from "react-redux"
-import { useListUsersMutation } from "../../store/apis/users"
+import { useDeleteMutation, useListUsersMutation } from "../../store/apis/users"
+import { Link } from "react-router-dom";
 
 const ListUsers = () => {
+	const [alert, setAlert] = useState({ "show": false })
+
 	const token = useSelector((state) => state.auth.token.access_token)
 	const [listUserHit, { isLoading, isSuccess, data }] = useListUsersMutation()
+	const [deleteHit, { isLoading: loadingDelete, isSuccess: successDelete, isError: errorDelete }] = useDeleteMutation()
+
+	let tempRole = []
+
+	const handleDelete = (e, id, role, name) => {
+		e.preventDefault()
+		if(role.includes('ROLE_SUPERUSER')) {
+			setAlert({
+				"variant": "danger",
+				"message": "Dilarang menghapus superadmin!",
+				"show": true
+			})
+			return
+		}
+		let confirm = window.confirm(`Apakah anda yakin ingin menghapus ${name}?`)
+		if (confirm) {
+			deleteHit({ token: token, id: id })
+		}
+	}
 
 	useEffect(() => {
 		listUserHit(token)
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [])
 
+	useEffect(() => {
+		if (successDelete) {
+			setAlert({
+				"variant": "success",
+				"message": "Berhasil menghapus user!",
+				"show": true
+			})
+			listUserHit(token)
+		}
+
+		if (errorDelete) {
+			setAlert({
+				"variant": "danger",
+				"message": "Gagal menghapus user!",
+				"show": true
+			})
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [loadingDelete])
+
 	return (
-		<Table striped hover size="sm">
-			<thead>
-				<tr>
-					<th>No</th>
-					<th>Nama</th>
-					<th>Role</th>
-					<th></th>
-				</tr>
-			</thead>
-			<tbody>
-				{
-					isLoading ?
-						<tr>
-							<td colSpan={4} className="text-center">Loading...</td>
-						</tr> :
-						isSuccess ?
-							data.data.map((el, i) => {
-								return (
-									<tr key={i}>
-										<td>1</td>
-										<th>{`${el.first_name} ${el.last_name}`}</th>
-										<th>Superadmin</th>
-										<th>
-											<ButtonGroup>
-												<Button href="/admin/users/2" size="sm" variant="primary">
-													detil
-												</Button>
-												<Button size="sm" variant="warning">
-													ubah
-												</Button>
-												<Button size="sm" variant="danger">
-													hapus
-												</Button>
-											</ButtonGroup>
-										</th>
-									</tr>
-								)
-							}) :
+		<>
+			{
+				alert.show ?
+					<Alert className="mt-3" variant={alert.variant} onClose={() => setAlert({ "show": false })} dismissible>
+						{alert.message}
+					</Alert> :
+					<></>
+			}
+			<Table striped hover size="sm" className="mt-3">
+				<thead>
+					<tr>
+						<th>No</th>
+						<th>Nama</th>
+						<th>Email</th>
+						<th>Role</th>
+						<th>Aktif</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					{
+						isLoading ?
 							<tr>
-								<td colSpan={4} className="text-center">Ambil data gagal</td>
-							</tr>
-				}
-			</tbody>
-		</Table>
+								<td colSpan={4} className="text-center">Loading...</td>
+							</tr> :
+							isSuccess ?
+								data.data.map((el, i) => {
+									tempRole = []
+									return (
+										<tr key={i}>
+											<td>{i + 1}</td>
+											<td>{`${el.first_name} ${el.last_name}`}</td>
+											<td>{el.email}</td>
+											<td>
+												{
+													el.role_name.map((ele, it) => {
+														if (!tempRole.includes(ele)) {
+															tempRole.push(ele)
+															if (ele === "ROLE_SUPERUSER") {
+																return (
+																	<span key={it}>Superadmin, </span>
+																)
+															} else if (ele === "ROLE_TN") {
+																return (
+																	<span key={it}>Penyewa, </span>
+																)
+															} else if (ele === "ROLE_SK") {
+																return (
+																	<span key={it}>Pencari, </span>
+																)
+															}
+														}
+														return <></>
+													})
+												}
+											</td>
+											<td>
+												{
+													el.enabled ?
+														<span>Aktif</span> :
+														<span>Belum aktif</span>
+												}
+											</td>
+											<td>
+												<ButtonGroup>
+													<Button as={Link} to={`/admin/users/${el.id}`} size="sm" variant="primary">
+														detil
+													</Button>
+													<Button size="sm" variant="warning">
+														ubah
+													</Button>
+													<Button size="sm" variant="danger" onClick={e => handleDelete(e, el.id, el.role_name, el.first_name + " " + el.last_name)}>
+														hapus
+													</Button>
+												</ButtonGroup>
+											</td>
+										</tr>
+									)
+								}) :
+								<tr>
+									<td colSpan={4} className="text-center">Ambil data gagal</td>
+								</tr>
+					}
+				</tbody>
+			</Table>
+		</>
 	)
 }
 
--- src/pages/admin/users/listuser.jsx
+++ src/pages/admin/users/listuser.jsx
@@ -2,14 +2,15 @@ import React from "react"
 import AdminLayout from "../../../layouts/admin.layout"
 import { Button, Container } from "react-bootstrap";
 import ListUsers from "../../../components/users/lists";
+import { Link } from "react-router-dom";
 
 function KelolaUser() {
 	return (
 		<>
 			<AdminLayout>
 				<Container className="mt-4">
-					<h1>List Users</h1>
-					<Button href="/admin/users/tambah">Tambah User</Button>
+					<h1 className="mt-3">List Users</h1>
+					<Button as={Link} className="mt-5" href="/admin/users/tambah">Tambah User</Button>
 					<ListUsers />
 				</Container>
 			</AdminLayout>
