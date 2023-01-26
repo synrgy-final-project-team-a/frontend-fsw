432a9f357c8d10ddaf66f32d6910c434821fc128
Wrendra57 <wrendra57@gmail.com>
2023-01-26 19:51:33 +0700

    integrasi api profile pencari

--- src/routes/pencari.js
+++ src/routes/pencari.js
@@ -1,41 +1,42 @@
-import React from "react"
-import Landing from "../pages/pencari/landing"
-import Profile from "../pages/pencari/profile/profilePencari"
-import InformasiPersonal from "../pages/pencari/profile/informasiPersonal"
-import EditProfile from "../pages/pencari/profile/editProfile"
+import React from "react";
+import Landing from "../pages/pencari/landing";
+import Profile from "../pages/pencari/profile/profilePencari";
+import InformasiPersonal from "../pages/pencari/profile/informasiPersonal";
+import EditProfile from "../pages/pencari/profile/editProfile";
 
 const PencariRoutes = [
-	{
-		name: "Home",
-		path: "/",
-		element: <Landing />,
-	},
-	{
-		name: "Cari Kos",
-		path: "/cari-kos",
-		element: <Landing />,
-	},
-	{
-		name: "Sewa Kos",
-		path: "/sewa-kos",
-		element: <Landing />,
-	},
-	{
-		path: "/profile",
-		children: [
-			{
-				path: "",
-				element: <Profile />,
-			},
-			{
-				path: "/informasi-personal",
-				element: <InformasiPersonal />,
-			},{
-				path: "/edit-profile",
-				element: <EditProfile />,
-			}
-		]
-	}
-]
+  {
+    name: "Home",
+    path: "/",
+    element: <Landing />,
+  },
+  {
+    name: "Cari Kos",
+    path: "/cari-kos",
+    element: <Landing />,
+  },
+  {
+    name: "Sewa Kos",
+    path: "/sewa-kos",
+    element: <Landing />,
+  },
+  {
+    path: "/profile",
+    children: [
+      {
+        path: "",
+        element: <Profile />,
+      },
+      {
+        path: "/informasi-personal",
+        element: <InformasiPersonal />,
+      },
+      {
+        path: "/edit-profile",
+        element: <EditProfile />,
+      },
+    ],
+  },
+];
 
-export default PencariRoutes
+export default PencariRoutes;
--- src/store/apis/users.js
+++ src/store/apis/users.js
@@ -1,36 +1,50 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const usersApi = createApi({
-	reducerPath: "usersApi",
-	baseQuery: fetchBaseQuery({ baseUrl: 'https://backend-fsw-development.up.railway.app/' }),
-	tagTypes: ['Users'],
-	endpoints: (build) => ({
-		listUsers: build.mutation({
-			query: (token) => ({
-				url: `api/users`,
-				method: 'GET',
-				headers: {
-					'authorization': `Bearer ${token}`
-				}
-			}),
-			invalidatesTags: ['Users'],
-		}),
-		currentUser: build.mutation({
-			query: (token) => ({
-				url: `api/user/detail`,
-				method: 'GET',
-				headers: {
-					'authorization': `Bearer ${token}`
-				}
-			}),
-			invalidatesTags: ['Users'],
-		}),
-	}),
-})
+  reducerPath: "usersApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://backend-fsw-development.up.railway.app/",
+  }),
+  tagTypes: ["Users"],
+  endpoints: (build) => ({
+    listUsers: build.mutation({
+      query: (token) => ({
+        url: `api/users`,
+        method: "GET",
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }),
+      invalidatesTags: ["Users"],
+    }),
+    currentUser: build.mutation({
+      query: (token) => ({
+        url: `api/user/detail`,
+        method: "GET",
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }),
+      invalidatesTags: ["Users"],
+    }),
+    editUser: build.mutation({
+      query: ({ token, body }) => ({
+        url: `/api/profile/edit`,
+        method: "PUT",
+        body: body,
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }),
+      invalidatesTags: ["Users"],
+    }),
+  }),
+});
 
 export const {
-	useListUsersMutation,
-	useCurrentUserMutation
-} = usersApi
+  useListUsersMutation,
+  useCurrentUserMutation,
+  useEditUserMutation,
+} = usersApi;
 
-export default usersApi
+export default usersApi;
