f1c4a637a3d44a211b1f16d0d14b3d1231c55b16
Rindho Ananta Samat <rindho.ananta@ti.ukdw.ac.id>
2023-01-26 13:49:36 +0700

    user list & detail

---
+++ src/components/kos/lists.jsx
@@ -0,0 +1,96 @@
+import { Badge, Button, Card, Col, Row } from "react-bootstrap"
+
+const ListKos = () => {
+	return (
+		<div className="mt-3">
+			<Row className="g-5">
+				<Col xs={12} lg={6}>
+					<Card className="flex-row">
+						<Card.Img src="/banner.png" />
+						<Card.Body className="d-flex flex-column">
+							<Card.Title>Kos Graha Mulia</Card.Title>
+							<Card.Text>
+								Jl.Kol Hadijanto No.117  Banaran, Sekaran, Semarang, Jawa Tenggah
+							</Card.Text>
+							<div>
+								<Badge bg="outline-primary">
+									Campur
+								</Badge>{' '}
+							</div>
+							<br />
+							<div className="d-flex flex-row-reverse">
+								<Button variant="secondary" size="sm" className="m-1">Edit</Button>
+								<Button variant="danger" size="sm" className="m-1">Hapus</Button>
+							</div>
+						</Card.Body>
+					</Card>
+				</Col>
+				<Col xs={12} lg={6}>
+					<Card className="flex-row">
+						<Card.Img src="/banner.png" />
+						<Card.Body className="d-flex flex-column">
+							<Card.Title>Kos Graha Mulia</Card.Title>
+							<Card.Text>
+								Jl.Kol Hadijanto No.117  Banaran, Sekaran, Semarang, Jawa Tenggah
+							</Card.Text>
+							<div>
+								<Badge bg="outline-primary">
+									Campur
+								</Badge>{' '}
+							</div>
+							<br />
+							<div className="d-flex flex-row-reverse">
+								<Button variant="secondary" size="sm" className="m-1">Edit</Button>
+								<Button variant="danger" size="sm" className="m-1">Hapus</Button>
+							</div>
+						</Card.Body>
+					</Card>
+				</Col>
+				<Col xs={12} lg={6}>
+					<Card className="flex-row">
+						<Card.Img src="/banner.png" />
+						<Card.Body className="d-flex flex-column">
+							<Card.Title>Kos Graha Mulia</Card.Title>
+							<Card.Text>
+								Jl.Kol Hadijanto No.117  Banaran, Sekaran, Semarang, Jawa Tenggah
+							</Card.Text>
+							<div>
+								<Badge bg="outline-primary">
+									Campur
+								</Badge>{' '}
+							</div>
+							<br />
+							<div className="d-flex flex-row-reverse">
+								<Button variant="secondary" size="sm" className="m-1">Edit</Button>
+								<Button variant="danger" size="sm" className="m-1">Hapus</Button>
+							</div>
+						</Card.Body>
+					</Card>
+				</Col>
+				<Col xs={12} lg={6}>
+					<Card className="flex-row">
+						<Card.Img src="/banner.png" />
+						<Card.Body className="d-flex flex-column">
+							<Card.Title>Kos Graha Mulia</Card.Title>
+							<Card.Text>
+								Jl.Kol Hadijanto No.117  Banaran, Sekaran, Semarang, Jawa Tenggah
+							</Card.Text>
+							<div>
+								<Badge bg="outline-primary">
+									Campur
+								</Badge>{' '}
+							</div>
+							<br />
+							<div className="d-flex flex-row-reverse">
+								<Button variant="secondary" size="sm" className="m-1">Edit</Button>
+								<Button variant="danger" size="sm" className="m-1">Hapus</Button>
+							</div>
+						</Card.Body>
+					</Card>
+				</Col>
+			</Row>
+		</div>
+	)
+}
+
+export default ListKos
