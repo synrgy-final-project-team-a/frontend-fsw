d6f7308763ae4c63839fb5b907e51a7bcd9d838e
Wrendra57 <wrendra57@gmail.com>
2023-01-27 11:27:06 +0700

    slicing pencari/pengajuan-sewa 1 & 2

--- src/routes/pencari.js
+++ src/routes/pencari.js
@@ -4,6 +4,8 @@ import DetailKos from "../pages/pencari/detailKos";
 import Profile from "../pages/pencari/profile/profilePencari";
 import InformasiPersonal from "../pages/pencari/profile/informasiPersonal";
 import EditProfile from "../pages/pencari/profile/editProfile";
+import PengajuanSewa1 from "../pages/pencari/pengajuanSewa1";
+import PengajuanSewa2 from "../pages/pencari/pengajuanSewa2";
 
 const PencariRoutes = [
   {
@@ -25,6 +27,20 @@ const PencariRoutes = [
     name: "Detail Kos",
     path: "/detail-kos",
     element: <DetailKos />,
+  },
+  {
+    name: "Pengajuan Sewa Kos",
+    path: "/pengajuan-sewa",
+    children: [
+      {
+        path: "/1",
+        element: <PengajuanSewa1 />,
+      },
+      {
+        path: "/2",
+        element: <PengajuanSewa2 />,
+      },
+    ],
   },
   {
     path: "/profile",
