166cfe731d6e37104ba35d235233b2a4002f5d3b
Rindho Ananta Samat <rindho.ananta@ti.ukdw.ac.id>
2023-01-31 18:57:12 +0700

    landing ui final

--- src/components/testimonial.jsx
+++ src/components/testimonial.jsx
@@ -1,66 +1,58 @@
-import { Card, Container, Row, Col } from "react-bootstrap";
+import { Card, Container } from "react-bootstrap";
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
 
 const Testimonial = () => {
-  return (
-    <Container className="my-5">
-      <h3 className="text-muted fw-bolder mb-3">Kata Mereka</h3>
-      <div className="">
-        <Row className="d-flex justify-content-between scrolling-wrapper">
-          <Col xs={8} lg={4} className="text-center">
-            <Card className="testi-card">
-              <Card.Body>
-                <Card.Text className="fw-bolder fs-6">“ Kosanku merupakan aplikasi pencari kos terbaik yang pernah saya gunakan, karena saya sangat dimudahkan dalam mencari kos dengan kualitas yang bagus “</Card.Text>
-                <Card.Text className="mb-1">
-                  <img
-                    src="https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg"
-                    className="img-fluid rounded-circle"
-                    style={{ width: "28px", height: "28px" }}
-                    alt=""
-                  />{" "}
-                  Andini Suryaningrum
-                </Card.Text>
-                <Card.Text>Mahasiswa Universitas Negeri Semarang</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col xs={8} lg={4} className="text-center">
-            <Card className="testi-card">
-              <Card.Body>
-                <Card.Text className="fw-bolder">“ Kosanku merupakan aplikasi pencari kos terbaik yang pernah saya gunakan, karena saya sangat dimudahkan dalam mencari kos dengan kualitas yang bagus “</Card.Text>
-                <Card.Text className="mb-1">
-                  <img
-                    src="https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg"
-                    className="img-fluid rounded-circle"
-                    style={{ width: "28px", height: "28px" }}
-                    alt=""
-                  />{" "}
-                  Andini Suryaningrum
-                </Card.Text>
-                <Card.Text>Mahasiswa Universitas Negeri Semarang</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col xs={8} lg={4} className="text-center">
-            <Card className="testi-card">
-              <Card.Body>
-                <Card.Text className="fw-bolder">“ Kosanku merupakan aplikasi pencari kos terbaik yang pernah saya gunakan, karena saya sangat dimudahkan dalam mencari kos dengan kualitas yang bagus “</Card.Text>
-                <Card.Text className="mb-1">
-                  <img
-                    src="https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg"
-                    className="img-fluid rounded-circle"
-                    style={{ width: "28px", height: "28px" }}
-                    alt=""
-                  />{" "}
-                  Andini Suryaningrum
-                </Card.Text>
-                <Card.Text>Mahasiswa Universitas Negeri Semarang</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </div>
-    </Container>
-  );
+	return (
+		<Container className="mb-5" id="testimoni">
+			<h2 className="mb-3">Kata Mereka</h2>
+			<OwlCarousel className="owl-theme" autoplay={true} items={3} loop dots={false} margin={20} >
+				<Card className="testimoni-card bg-outline-primary">
+					<Card.Body className="text-center">
+						<Card.Text className="fw-bolder fs-6">"Kosanku merupakan aplikasi pencari kos terbaik yang pernah saya gunakan, karena saya sangat dimudahkan dalam mencari kos dengan kualitas yang bagus"</Card.Text>
+						<div className="d-flex justify-content-center align-items-center mb-2">
+							<img
+								src="https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg"
+								className="img-fluid rounded-circle"
+								alt=""
+							/>
+							<span className="fw-bold px-2">Andini Suryaningrum</span>
+						</div>
+						<Card.Text>Mahasiswa Universitas Negeri Semarang</Card.Text>
+					</Card.Body>
+				</Card>
+				<Card className="testimoni-card bg-outline-primary">
+					<Card.Body className="text-center">
+						<Card.Text className="fw-bolder fs-6">"Kosanku merupakan aplikasi pencari kos terbaik yang pernah saya gunakan, karena saya sangat dimudahkan dalam mencari kos dengan kualitas yang bagus"</Card.Text>
+						<div className="d-flex justify-content-center align-items-center mb-2">
+							<img
+								src="https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg"
+								className="img-fluid rounded-circle"
+								alt=""
+							/>
+							<span className="fw-bold px-2">Andini Suryaningrum</span>
+						</div>
+						<Card.Text>Mahasiswa Universitas Negeri Semarang</Card.Text>
+					</Card.Body>
+				</Card>
+				<Card className="testimoni-card bg-outline-primary">
+					<Card.Body className="text-center">
+						<Card.Text className="fw-bolder fs-6">"Kosanku merupakan aplikasi pencari kos terbaik yang pernah saya gunakan, karena saya sangat dimudahkan dalam mencari kos dengan kualitas yang bagus"</Card.Text>
+						<div className="d-flex justify-content-center align-items-center mb-2">
+							<img
+								src="https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg"
+								className="img-fluid rounded-circle"
+								alt=""
+							/>
+							<span className="fw-bold px-2">Andini Suryaningrum</span>
+						</div>
+						<Card.Text>Mahasiswa Universitas Negeri Semarang</Card.Text>
+					</Card.Body>
+				</Card>
+			</OwlCarousel>
+		</Container>
+	);
 };
 
 export default Testimonial;
