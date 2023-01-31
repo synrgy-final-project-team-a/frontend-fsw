e070923a096fb5c13496431cefe35998f75422a8
Rindho Ananta Samat <73924542+yerndd@users.noreply.github.com>
2023-01-31 12:56:53 +0700

    landing navbar fix (#52)
    * add :package: bootstrap & react-bootstrap
    
    * change to scss
    
    * add routes in App.jsx
    
    * add owl carousel
    
    * edit owl carousel
    
    * deploy vercel
    
    * deploy vercel
    
    * add kostByCity
    
    * add KostByCampus
    
    * deploy fix
    
    * fix deploy
    
    * add footer component
    
    * search
    
    * fix banner & kostBy
    
    * add footer
    
    * delete import row,col,form in search.jsx
    
    * change
    
    * change
    
    * edit sedikit navbar
    
    * add testimonial & yg lain
    
    * edit navbar
    
    * ada confloct
    
    * search
    
    * fix bestKost & kostByLocation
    
    * fix testi card to become center
    
    * add pencari layout
    
    * add list users and detail
    
    * routes admin test
    
    * add tambah user page
    
    * edit tambah user page
    
    * edit navbar component
    
    * edit navbar (belom fix) and footer
    
    * (reja): add responsive on banner
    
    * edit tombol login and search bar
    
    * add className btn-login at button login
    
    * edit footer and search components
    
    * testi hifi & responsive done
    
    * lanjut kostbylocation
    
    * edit bestKost component
    
    * fix conflict at variable.css
    
    * resolve conflict
    
    * landing bestKost comment
    
    * remove yarn.lock
    
    ---------
    
    Co-authored-by: Ryan Jonathan <ryanjnthan@gmail.com>
    Co-authored-by: fayyadhsr <outofthisworldyo@gmail.com>
    Co-authored-by: Reja <rezanurrochmat@gmail.com>

--- src/components/bestKost.jsx
+++ src/components/bestKost.jsx
@@ -1,4 +1,3 @@
-import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Badge from "react-bootstrap/Badge";
 import Row from "react-bootstrap/Row";
@@ -7,169 +6,128 @@ import Container from "react-bootstrap/Container";
 
 function BestKost() {
   return (
-    <Container>
-      <div>
-        <h2>Kos Terbaik dari Kami</h2>
-
-        <Row lg={3}>
-          <Col className="d-flex">
-            <Card className="mb-2 flex-fill">
-              <Card.Img variant="top" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" />
-              <Card.Body>
-                <Badge bg="secondary" text="light">
-                  Putri
-                </Badge>{" "}
-                <Badge bg="secondary" text="light">
-                  Kos Ajimumpung
-                </Badge>{" "}
-                {/* <Card.Title>Card Title</Card.Title> */}
-                <Card.Text className="">
-                  <p style={{ margin: 0 }}>Indekos Bu Sapri</p>
-                  <p style={{ margin: 0 }} className="fw-bold">
-                    Jakarta
-                  </p>
-                  <p style={{ margin: 0, fontSize: "12px" }} className="text-muted">
-                    K. Mandi Dalam, Kloset Duduk
+    <Container fluid>
+      <div className="px-5 ">
+      <h2 className="section-title ps-3">Kos Terbaik dari Kami</h2>
+      
+        <Row lg={3} className="g-2 px-1">
+          <Col className="d-flex px-3">
+            <Card className="mb-2 flex-fill border-0 px-1">
+              <Card.Img variant="top" src="/kos-giya-putri.png" />
+              <Card.Body className="kost-desc pe-0">
+                <Card.Title className="kost-title">Kos Giya Putri</Card.Title>
+                <Card.Text className="mb-1">
+                  <p className="kost-location m-0">Cempaka Timur, Semarang</p>
+                  <p className="kost-price m-0">
+                    Rp. 1.500.000 / <span style={{color: "#8E9191"}}>Bulan</span>
                   </p>
                 </Card.Text>
-                <Button size="sm" variant="primary">
-                  Lihat Detail
-                </Button>
+                <Badge className="kost-category-badge" bg="none" text="primary">
+                  <span><img src="/woman.png" alt="" /></span>
+                  Putri
+                </Badge>{" "}
+                <a href="#" ><img src="/like.png" style={{ marginLeft: '70%' }} alt="" /></a>
               </Card.Body>
             </Card>
           </Col>
-          <Col className="d-flex">
-            <Card className="ms-2 mb-2 flex-fill">
-              <Card.Img variant="top" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" />
-              <Card.Body>
-                <Badge bg="secondary" text="light">
-                  Putri
-                </Badge>{" "}
-                <Badge bg="secondary" text="light">
-                  Kos Ajimumpung
-                </Badge>{" "}
-                {/* <Card.Title>Card Title</Card.Title> */}
-                <Card.Text className="">
-                  <p style={{ margin: 0 }}>Indekos Bu Sapri</p>
-                  <p style={{ margin: 0 }} className="fw-bold">
-                    Jakarta
-                  </p>
-                  <p style={{ margin: 0, fontSize: "12px" }} className="text-muted">
-                    K. Mandi Dalam, Kloset Duduk
+          <Col className="d-flex px-3">
+            <Card className="mb-2 flex-fill border-0 px-1">
+              <Card.Img variant="top" src="/kos-turiman.png" />
+              <Card.Body className="kost-desc pe-0">
+                <Card.Title className="kost-title">Kos H.Turiman Banaran</Card.Title>
+                <Card.Text className="mb-1">
+                  <p className="kost-location m-0">Jl.Banaran No.117 Banaran Sekaran, Semarang</p>
+                  <p className="kost-price m-0">
+                    Rp. 850.000 / <span style={{color: "#8E9191"}}>Bulan</span>
                   </p>
                 </Card.Text>
-                <Button size="sm" variant="primary">
-                  Lihat Detail
-                </Button>
+                <Badge className="kost-category-badge" bg="none" text="primary">
+                  <span><img src="/people.png" alt="" /></span>
+                  Putri
+                </Badge>{" "}
+                <a href="#" ><img src="/like.png" style={{ marginLeft: '70%' }} alt="" /></a>
               </Card.Body>
             </Card>
           </Col>
-          <Col className="d-flex">
-            <Card className="ms-2 mb-2 flex-fill">
-              <Card.Img variant="top" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" />
-              <Card.Body>
-                <Badge bg="secondary" text="light">
-                  Putri
-                </Badge>{" "}
-                <Badge bg="secondary" text="light">
-                  Kos Ajimumpung
-                </Badge>{" "}
-                {/* <Card.Title>Card Title</Card.Title> */}
-                <Card.Text className="">
-                  <p style={{ margin: 0 }}>Indekos Bu Sapri</p>
-                  <p style={{ margin: 0 }} className="fw-bold">
-                    Jakarta
-                  </p>
-                  <p style={{ margin: 0, fontSize: "12px" }} className="text-muted">
-                    K. Mandi Dalam, Kloset Duduk
+          <Col className="d-flex px-3">
+            <Card className="mb-2 flex-fill border-0 px-1">
+              <Card.Img variant="top" src="/kos-mambo.png" />
+              <Card.Body className="kost-desc pe-0">
+                <Card.Title className="kost-title">Kos MAMBO 23 DUREN TIGA</Card.Title>
+                <Card.Text className="mb-1">
+                  <p className="kost-location m-0">Jl. Prof. Huda No.76 Banyumanik, Banyuwangi</p>
+                  <p className="kost-price m-0">
+                    Rp. 750.000 / <span style={{color: "#8E9191"}}>Bulan</span>
                   </p>
                 </Card.Text>
-                <Button size="sm" variant="primary">
-                  Lihat Detail
-                </Button>
+                <Badge className="kost-category-badge" bg="none" text="primary">
+                  <span><img src="/man.png" alt="" /></span>
+                  Putri
+                </Badge>{" "}
+                <a href="#" ><img src="/like.png" style={{ marginLeft: '70%' }} alt="" /></a>
               </Card.Body>
             </Card>
           </Col>
-          <Col className="d-flex">
-            <Card className="ms-2 mb-2 flex-fill">
-              <Card.Img variant="top" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" />
-              <Card.Body>
-                <Badge bg="secondary" text="light">
-                  Putri
-                </Badge>{" "}
-                <Badge bg="secondary" text="light">
-                  Kos Ajimumpung
-                </Badge>{" "}
-                {/* <Card.Title>Card Title</Card.Title> */}
-                <Card.Text className="">
-                  <p style={{ margin: 0 }}>Indekos Bu Sapri</p>
-                  <p style={{ margin: 0 }} className="fw-bold">
-                    Jakarta
-                  </p>
-                  <p style={{ margin: 0, fontSize: "12px" }} className="text-muted">
-                    K. Mandi Dalam, Kloset Duduk
+          <Col className="d-flex px-3">
+            <Card className="mb-2 flex-fill border-0 px-1">
+              <Card.Img variant="top" src="/kos-bunda-rina.png" />
+              <Card.Body className="kost-desc pe-0">
+                <Card.Title className="kost-title">Kos Bunda Rina Kalimasada</Card.Title>
+                <Card.Text className="mb-1">
+                  <p className="kost-location m-0">Kecoklat Barat D-15, Kota Surabaya</p>
+                  <p className="kost-price m-0">
+                    Rp. 1.099.000 / <span style={{color: "#8E9191"}}>Bulan</span>
                   </p>
                 </Card.Text>
-                <Button size="sm" variant="primary">
-                  Lihat Detail
-                </Button>
+                <Badge className="kost-category-badge" bg="none" text="primary">
+                  <span><img src="/people.png" alt="" /></span>
+                  Putri
+                </Badge>{" "}
+                <a href="#" ><img src="/like.png" style={{ marginLeft: '70%' }} alt="" /></a>
               </Card.Body>
             </Card>
           </Col>
-          <Col className="d-flex">
-            <Card className="ms-2 mb-2 flex-fill">
-              <Card.Img variant="top" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" />
-              <Card.Body>
-                <Badge bg="secondary" text="light">
-                  Putri
-                </Badge>{" "}
-                <Badge bg="secondary" text="light">
-                  Kos Ajimumpung
-                </Badge>{" "}
-                {/* <Card.Title>Card Title</Card.Title> */}
-                <Card.Text className="">
-                  <p style={{ margin: 0 }}>Indekos Bu Sapri</p>
-                  <p style={{ margin: 0 }} className="fw-bold">
-                    Jakarta
-                  </p>
-                  <p style={{ margin: 0, fontSize: "12px" }} className="text-muted">
-                    K. Mandi Dalam, Kloset Duduk
+          <Col className="d-flex px-3">
+            <Card className="mb-2 flex-fill border-0 px-1">
+              <Card.Img variant="top" src="/agatha.png" />
+              <Card.Body className="kost-desc pe-0">
+                <Card.Title className="kost-title">Agatha Adam Malik, Bandung</Card.Title>
+                <Card.Text className="mb-1">
+                  <p className="kost-location m-0">Tanjung Sari, Bandung Barat</p>
+                  <p className="kost-price m-0">
+                    Rp. 1.500.000 / <span style={{color: "#8E9191"}}>Bulan</span>
                   </p>
                 </Card.Text>
-                <Button size="sm" variant="primary">
-                  Lihat Detail
-                </Button>
+                <Badge className="kost-category-badge" bg="none" text="primary">
+                  <span><img src="/man.png" alt="" /></span>
+                  Putri
+                </Badge>{" "}
+                <a href="#" style={{position: "relative", left:"15.5rem"}}><img src="/like.png" style={{ marginLeft: '70%' }} alt="" /></a>
               </Card.Body>
             </Card>
           </Col>
-          <Col className="d-flex">
-            <Card className="ms-2 mb-2 flex-fill">
-              <Card.Img variant="top" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=-1&s=1" />
-              <Card.Body>
-                <Badge bg="secondary" text="light">
-                  Putri
-                </Badge>{" "}
-                <Badge bg="secondary" text="light">
-                  Kos Ajimumpung
-                </Badge>{" "}
-                {/* <Card.Title>Card Title</Card.Title> */}
-                <Card.Text className="">
-                  <p style={{ margin: 0 }}>Indekos Bu Sapri</p>
-                  <p style={{ margin: 0 }} className="fw-bold">
-                    Jakarta
-                  </p>
-                  <p style={{ margin: 0, fontSize: "12px" }} className="text-muted">
-                    K. Mandi Dalam, Kloset Duduk
+          <Col className="d-flex px-3">
+            <Card className="mb-2 flex-fill border-0 px-1">
+              <Card.Img variant="top" src="/grand-luxury.png" />
+              <Card.Body className="kost-desc pe-0">
+                <Card.Title className="kost-title">Grand Luxury Balemong Barat</Card.Title>
+                <Card.Text className="mb-1">
+                  <p className="kost-location m-0">Jl. Pakintelan 34, Jakarta Utara</p>
+                  <p className="kost-price m-0">
+                    Rp. 1.830.000 / <span style={{color: "#8E9191"}}>Bulan</span>
                   </p>
                 </Card.Text>
-                <Button size="sm" variant="primary">
-                  Lihat Detail
-                </Button>
+                <Badge className="kost-category-badge" bg="none" text="primary">
+                  <span><img src="/woman.png" alt="" /></span>
+                  Putri
+                </Badge>{" "}
+                <a href="#" ><img src="/like.png" style={{ marginLeft: '70%' }} alt="" /></a>
               </Card.Body>
             </Card>
           </Col>
         </Row>
-      </div>
+           
+    </div>
     </Container>
   );
 }
--- src/components/kostByLocation.jsx
+++ src/components/kostByLocation.jsx
@@ -1,134 +1,237 @@
 import Card from "react-bootstrap/Card";
+import { Container, Row, Col } from "react-bootstrap";
 
 const KostByLocation = () => {
-	return (
-		<div className="container">
-			<p>Cari Kosan Berdasarkan Lokasi</p>
-			<div className="row">
-				<div className="col-4">
-					<div className="container">
-						<div className="row">
-							<div className="col">
-								<table className="table">
-									<tbody>
-										<tr>
-											<th scope="row">1</th>
-											<td>Bandung</td>
-										</tr>
-										<tr>
-											<th scope="row">2</th>
-											<td>Bandung</td>
-										</tr>
-										<tr>
-											<th scope="row">3</th>
-											<td>Bandung</td>
-										</tr>
-										<tr>
-											<th scope="row">3</th>
-											<td>Bandung</td>
-										</tr>
-									</tbody>
-								</table>
-							</div>
-							<div className="col">
-								<table className="table">
-									<tbody>
-										<tr>
-											<th scope="row">1</th>
-											<td>Bandung</td>
-										</tr>
-										<tr>
-											<th scope="row">2</th>
-											<td>Bandung</td>
-										</tr>
-										<tr>
-											<th scope="row">3</th>
-											<td>Bandung</td>
-										</tr>
-										<tr>
-											<th scope="row">3</th>
-											<td>Bandung</td>
-										</tr>
-									</tbody>
-								</table>
-							</div>
-						</div>
-					</div>
-				</div>
+  return (
+    <Container>
+      <Row className="d-flex">
+        <Col>
+          <div className="d-flex flex-column">
+            <div>
+              <h3 className="text-muted fw-bolder mb-3">Cari Kosan Dari Lokasi</h3>
+            </div>
+            <Row>
+              <Col lg={6} className="d-flex justify-content-between flex-column">
+                <Card className="shadow-sm mb-2">
+                  <Card.Text className="my-1 d-flex justify-content-evenly">
+                    <img
+                      src="https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg"
+                      className="img-fluid rounded-circle"
+                      style={{ width: "28px", height: "28px" }}
+                      alt=""
+                    />{" "}
+                    Bandung
+                  </Card.Text>
+                </Card>
+                <Card className="shadow-sm mb-2">
+                  <Card.Text className="my-1 d-flex justify-content-evenly">
+                    <img
+                      src="https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg"
+                      className="img-fluid rounded-circle"
+                      style={{ width: "28px", height: "28px" }}
+                      alt=""
+                    />{" "}
+                    Bandung
+                  </Card.Text>
+                </Card>
+                <Card className="shadow-sm mb-2">
+                  <Card.Text className="my-1 d-flex justify-content-evenly">
+                    <img
+                      src="https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg"
+                      className="img-fluid rounded-circle"
+                      style={{ width: "28px", height: "28px" }}
+                      alt=""
+                    />{" "}
+                    Bandung
+                  </Card.Text>
+                </Card>
+                <Card className="shadow-sm">
+                  <Card.Text className="my-1 d-flex justify-content-evenly">
+                    <img
+                      src="https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg"
+                      className="img-fluid rounded-circle"
+                      style={{ width: "28px", height: "28px" }}
+                      alt=""
+                    />{" "}
+                    Bandung
+                  </Card.Text>
+                </Card>
+              </Col>
+              <Col lg={6} className="d-flex justify-content-between flex-column">
+                <Card className="shadow-sm mb-2">
+                  <Card.Text className="my-1 d-flex justify-content-evenly">
+                    <img
+                      src="https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg"
+                      className="img-fluid rounded-circle"
+                      style={{ width: "28px", height: "28px" }}
+                      alt=""
+                    />{" "}
+                    Bandung
+                  </Card.Text>
+                </Card>
+                <Card className="shadow-sm mb-2">
+                  <Card.Text className="my-1 d-flex justify-content-evenly">
+                    <img
+                      src="https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg"
+                      className="img-fluid rounded-circle"
+                      style={{ width: "28px", height: "28px" }}
+                      alt=""
+                    />{" "}
+                    Bandung
+                  </Card.Text>
+                </Card>
+                <Card className="shadow-sm mb-2">
+                  <Card.Text className="my-1 d-flex justify-content-evenly">
+                    <img
+                      src="https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg"
+                      className="img-fluid rounded-circle"
+                      style={{ width: "28px", height: "28px" }}
+                      alt=""
+                    />{" "}
+                    Bandung
+                  </Card.Text>
+                </Card>
+                <Card className="shadow-sm">
+                  <Card.Text className="my-1 d-flex justify-content-evenly">
+                    <img
+                      src="https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg"
+                      className="img-fluid rounded-circle"
+                      style={{ width: "28px", height: "28px" }}
+                      alt=""
+                    />{" "}
+                    Bandung
+                  </Card.Text>
+                </Card>
+              </Col>
+            </Row>
+          </div>
+        </Col>
 
-				<div className="col-2">
-					<div className="container">
-						<Card>
-							<Card.Img variant="top" src="https://png.pngtree.com/png-vector/20191120/ourmid/pngtree-bandung-landmark-icon-west-java-indonesia-png-image_2008327.jpg" />
-							<Card.Body>
-								<Card.Title className="text-center">KOTA</Card.Title>
-							</Card.Body>
-						</Card>
-					</div>
-				</div>
-				<div className="col-2">
-					<div className="container">
-						<Card>
-							<Card.Img variant="top" src="https://rekreartive.com/wp-content/uploads/2018/11/Logo-Untan-Universitas-Tanjungpura-PNG.png" />
-							<Card.Body>
-								<Card.Title className="text-center">KAMPUS</Card.Title>
-							</Card.Body>
-						</Card>
-					</div>
-				</div>
-				<div className="col-4">
-					<div className="container">
-						<div className="row">
-							<div className="col">
-								<table className="table">
-									<tbody>
-										<tr>
-											<th scope="row">1</th>
-											<td>UI-Depok</td>
-										</tr>
-										<tr>
-											<th scope="row">2</th>
-											<td>UI-Depok</td>
-										</tr>
-										<tr>
-											<th scope="row">3</th>
-											<td>UI-Depok</td>
-										</tr>
-										<tr>
-											<th scope="row">3</th>
-											<td>UI-Depok</td>
-										</tr>
-									</tbody>
-								</table>
-							</div>
-							<div className="col">
-								<table className="table">
-									<tbody>
-										<tr>
-											<th scope="row">1</th>
-											<td>UI-Depok</td>
-										</tr>
-										<tr>
-											<th scope="row">2</th>
-											<td>UI-Depok</td>
-										</tr>
-										<tr>
-											<th scope="row">3</th>
-											<td>UI-Depok</td>
-										</tr>
-										<tr>
-											<th scope="row">3</th>
-											<td>UI-Depok</td>
-										</tr>
-									</tbody>
-								</table>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
-}
+        <Col lg={2} className="d-none d-lg-flex">
+          <div className="container">
+            <Card>
+              <Card.Img variant="top" src="https://png.pngtree.com/png-vector/20191120/ourmid/pngtree-bandung-landmark-icon-west-java-indonesia-png-image_2008327.jpg" />
+              <Card.Body>
+                <Card.Title className="text-center">KOTA</Card.Title>
+              </Card.Body>
+            </Card>
+          </div>
+        </Col>
+        <Col lg={2} className="d-none d-lg-flex">
+          <div className="container">
+            <Card>
+              <Card.Img variant="top" src="https://rekreartive.com/wp-content/uploads/2018/11/Logo-Untan-Universitas-Tanjungpura-PNG.png" />
+              <Card.Body>
+                <Card.Title className="text-center">KAMPUS</Card.Title>
+              </Card.Body>
+            </Card>
+          </div>
+        </Col>
+
+        <Col>
+          <div className="d-flex flex-column">
+            <div>
+              <h3 className="text-muted fw-bolder mb-3">Cari Kosan Dari Kampus</h3>
+            </div>
+            <Row>
+              <Col lg={6} className="d-flex justify-content-between flex-column">
+                <Card className="shadow-sm mb-2">
+                  <Card.Text className="my-1 d-flex justify-content-evenly">
+                    <img
+                      src="https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg"
+                      className="img-fluid rounded-circle"
+                      style={{ width: "28px", height: "28px" }}
+                      alt=""
+                    />{" "}
+                    Bandung
+                  </Card.Text>
+                </Card>
+                <Card className="shadow-sm mb-2">
+                  <Card.Text className="my-1 d-flex justify-content-evenly">
+                    <img
+                      src="https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg"
+                      className="img-fluid rounded-circle"
+                      style={{ width: "28px", height: "28px" }}
+                      alt=""
+                    />{" "}
+                    Bandung
+                  </Card.Text>
+                </Card>
+                <Card className="shadow-sm mb-2">
+                  <Card.Text className="my-1 d-flex justify-content-evenly">
+                    <img
+                      src="https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg"
+                      className="img-fluid rounded-circle"
+                      style={{ width: "28px", height: "28px" }}
+                      alt=""
+                    />{" "}
+                    Bandung
+                  </Card.Text>
+                </Card>
+                <Card className="shadow-sm">
+                  <Card.Text className="my-1 d-flex justify-content-evenly">
+                    <img
+                      src="https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg"
+                      className="img-fluid rounded-circle"
+                      style={{ width: "28px", height: "28px" }}
+                      alt=""
+                    />{" "}
+                    Bandung
+                  </Card.Text>
+                </Card>
+              </Col>
+              <Col lg={6} className="d-flex justify-content-between flex-column">
+                <Card className="shadow-sm mb-2">
+                  <Card.Text className="my-1 d-flex justify-content-evenly">
+                    <img
+                      src="https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg"
+                      className="img-fluid rounded-circle"
+                      style={{ width: "28px", height: "28px" }}
+                      alt=""
+                    />{" "}
+                    Bandung
+                  </Card.Text>
+                </Card>
+                <Card className="shadow-sm mb-2">
+                  <Card.Text className="my-1 d-flex justify-content-evenly">
+                    <img
+                      src="https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg"
+                      className="img-fluid rounded-circle"
+                      style={{ width: "28px", height: "28px" }}
+                      alt=""
+                    />{" "}
+                    Bandung
+                  </Card.Text>
+                </Card>
+                <Card className="shadow-sm mb-2">
+                  <Card.Text className="my-1 d-flex justify-content-evenly">
+                    <img
+                      src="https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg"
+                      className="img-fluid rounded-circle"
+                      style={{ width: "28px", height: "28px" }}
+                      alt=""
+                    />{" "}
+                    Bandung
+                  </Card.Text>
+                </Card>
+                <Card className="shadow-sm">
+                  <Card.Text className="my-1 d-flex justify-content-evenly">
+                    <img
+                      src="https://pub-static.fotor.com/assets/projects/pages/d5bdd0513a0740a8a38752dbc32586d0/fotor-03d1a91a0cec4542927f53c87e0599f6.jpg"
+                      className="img-fluid rounded-circle"
+                      style={{ width: "28px", height: "28px" }}
+                      alt=""
+                    />{" "}
+                    Bandung
+                  </Card.Text>
+                </Card>
+              </Col>
+            </Row>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default KostByLocation;
